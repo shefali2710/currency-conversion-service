3eb212ec91fd773be32c2740e006004713aeeac0
iasinfobeans <shefali.garg@infobeans.com>
2019-01-29 13:01:39 +0530

    pushing conversion service to git repo

---
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,27 @@
+package com.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableFeignClients("com.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+  }
+  
+  
+  //this is code for zipkin monitoring
+  @Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
+
